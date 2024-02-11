54f9cf43105b07fb9306789b3f3fb11b53e6acb8
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-02-11 15:41:53 -0700

    feat: Added Example Supertest testing workflow

---
+++ src/user/tests/user.http.spec.ts
@@ -0,0 +1,14 @@
+// Test /users endpoint as an example
+
+import { getSuperTestInstance } from "@/lib/test/test";
+
+const request = getSuperTestInstance();
+
+describe("GET /api/users", () => {
+  it("returns a status code of 200", async () => {
+    const res = await request.get("/api/users").send();
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ message: "Welcome to the users endpoint !" });
+  });
+});
