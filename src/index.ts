45bf53c2523010d95356771151effd45b931d127
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-02-11 15:07:01 -0700

    feat: setup dummy routes + express server singleton

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 // The main entry point of the backend part of our CarbonCopy app
-import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { getExpressServerInstance } from "./lib/server/server";
 
 // Load our environment variables from .env
 // file using the dotenv npm package
 dotenv.config();
 
-const app: Express = express();
+const app = getExpressServerInstance();
 
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Carbon Copy Backend Server!");
-});
 
 app.listen(port, () => {
   console.log(
