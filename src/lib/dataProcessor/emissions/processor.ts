c28a6696957cdd066a3b52ca22153d3e839de97c
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-04-20 20:57:52 -0600

    feat: Setup models, parsing lib and parser function

---
+++ src/lib/dataProcessor/emissions/processor.ts
@@ -0,0 +1,3 @@
+// TODO: remove "eslint-disable-next-line no-unused-vars" from parser.config.ts and model.ts after their usage
+// TODO: Two functions => processGasolineData(), processEVData()
+// TODO: Functions will pass transformHeader, complete and error callbacks
---
+++ src/lib/dataProcessor/parser.config.ts
@@ -0,0 +1,8 @@
+import { ParseConfig } from "papaparse";
+
+// eslint-disable-next-line no-unused-vars
+export const papaParseConfig: ParseConfig = {
+  delimiter: ",",
+  header: true,
+  dynamicTyping: true,
+};
