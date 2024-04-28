e907b7ce1d287fd109e7ccccc9b93265c1bcc1d3
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-04-28 16:35:05 -0600

    feat: API - Yearly emissions for a given make, model and vehicle type

--- src/emissions/queries/emissions.queries.ts
+++ src/emissions/queries/emissions.queries.ts
@@ -8,3 +8,20 @@ export const buildAverageEmissionsYearlyQuery = (vehicleType: string) => {
     ORDER BY year
   `;
 };
+
+export const buildAverageEmissionsForMakeAndModelYearlyQuery = (
+  make: string,
+  model: string,
+  vehicleType: string,
+) => {
+  return `
+    SELECT year, ROUND(AVG(emissions_per_mile)) AS avg_emission_in_grams
+    FROM model
+    WHERE year BETWEEN 2012 and 2024
+    AND car_make='${make}'
+    AND model = '${model}'
+    AND vehicle_type='${vehicleType}'
+    GROUP BY year
+    ORDER BY year
+  `;
+};
--- src/emissions/routes.ts
+++ src/emissions/routes.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from "express";
 import { isValidVehicleType } from "../vehicles/vehicles.utils";
-import { buildAverageEmissionsYearlyQuery } from "./queries/emissions.queries";
+import {
+  buildAverageEmissionsForMakeAndModelYearlyQuery,
+  buildAverageEmissionsYearlyQuery,
+} from "./queries/emissions.queries";
 import { connect } from "../lib/db/db";
 import { VEHICLE_TYPES } from "../lib/constants";
 
@@ -59,6 +62,61 @@ emissionsRouter.get("/yearly", async (req: Request, res: Response) => {
     );
 
     errorMsg = `Unexpected error occurred while retrieving yearly emissions for vehicle_type ${vehicleType}`;
+
+    return res.status(500).json({ data: [], error: errorMsg });
+  } finally {
+    await close();
+  }
+});
+
+emissionsRouter.get("/yearlyByMake", async (req: Request, res: Response) => {
+  const vehicleType = (req?.query?.vehicleType || null) as string;
+  const make: string = (req?.query?.make || null) as string;
+  const model: string = (req?.query?.model || null) as string;
+
+  const miles = parseInt(req?.query?.miles as string) || 50;
+
+  let errorMsg = "";
+
+  if (!isValidVehicleType(vehicleType)) {
+    errorMsg = `Vehicle type not found (or) ${vehicleType} is not a valid type`;
+    return res.status(400).json({ data: [], error: errorMsg });
+  } else if (!make || !model) {
+    errorMsg = `Vehicle make / model not found (or) ${make} / ${model} is not a valid make/model`;
+    return res.status(400).json({ data: [], error: errorMsg });
+  }
+
+  const yearlyMakeModelEmissionsQuery =
+    buildAverageEmissionsForMakeAndModelYearlyQuery(make, model, vehicleType);
+
+  // console.log(yearlyMakeModelEmissionsQuery);
+
+  const { query, close } = await connect();
+
+  try {
+    const dbResponse = await query(yearlyMakeModelEmissionsQuery);
+
+    console.log(
+      `Retrieved ${dbResponse.rows.length} avg yearly emission records for make: 
+        ${make}, model: ${model}, vehicle type: ${vehicleType}`,
+    );
+
+    const data = dbResponse.rows.map((row) => {
+      return {
+        year: row.year,
+        emissions: row.avg_emission_in_grams * miles,
+      };
+    });
+
+    return res.status(200).json({ data, error: "" });
+  } catch (error) {
+    console.log(
+      `Error while retrieving avg yearly emission records for make: 
+        ${make}, model: ${model}, vehicle type: ${vehicleType} - ${error}`,
+    );
+
+    errorMsg = `Unexpected error occurred while retrieving avg yearly emission 
+      records for make: ${make}, model: ${model}, vehicle type: ${vehicleType}`;
 
     return res.status(500).json({ data: [], error: errorMsg });
   } finally {
