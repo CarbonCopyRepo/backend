7019fd6564e9d02064fc704098c7f274a1085281
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-04-28 18:45:29 -0600

    feat: API - Avg yearly fuel consumption by model and make

--- src/fuel-consumption/queries/fuel-consumption.queries.ts
+++ src/fuel-consumption/queries/fuel-consumption.queries.ts
@@ -4,12 +4,30 @@ export const buildAvgYearlyConsumptionQuery = (vehicleType: string) => {
   const columnName =
     vehicleType === VEHICLE_TYPES.EV ? "energy_per_100_km" : "miles_per_gallon";
 
-  console.log(columnName);
+  return `
+    SELECT year, ROUND(AVG(${columnName})) AS avg_consumption
+    FROM model
+    WHERE year BETWEEN 2012 and 2024
+    AND vehicle_type = '${vehicleType}'
+    GROUP BY year
+    ORDER BY year
+  `;
+};
+
+export const buildAvgYearlyConsumptionForMakeAndModelQuery = (
+  make: string,
+  model: string,
+  vehicleType: string,
+) => {
+  const columnName =
+    vehicleType === VEHICLE_TYPES.EV ? "energy_per_100_km" : "miles_per_gallon";
 
   return `
     SELECT year, ROUND(AVG(${columnName})) AS avg_consumption
     FROM model
     WHERE year BETWEEN 2012 and 2024
+    AND car_make='${make}'
+    AND model = '${model}'
     AND vehicle_type = '${vehicleType}'
     GROUP BY year
     ORDER BY year
--- src/fuel-consumption/routes.ts
+++ src/fuel-consumption/routes.ts
@@ -4,7 +4,10 @@ import { connect } from "../lib/db/db";
 import { isValidVehicleType } from "../vehicles/vehicles.utils";
 import { convertToBTUs } from "./fuel-consumption.utils";
 
-import { buildAvgYearlyConsumptionQuery } from "./queries/fuel-consumption.queries";
+import {
+  buildAvgYearlyConsumptionForMakeAndModelQuery,
+  buildAvgYearlyConsumptionQuery,
+} from "./queries/fuel-consumption.queries";
 
 const fuelConsumptionRouter = Router();
 
@@ -59,5 +62,67 @@ fuelConsumptionRouter.get("/yearly", async (req: Request, res: Response) => {
     await close();
   }
 });
+
+fuelConsumptionRouter.get(
+  "/yearlyByMake",
+  async (req: Request, res: Response) => {
+    const vehicleType = (req?.query?.vehicleType || null) as string;
+    const make: string = (req?.query?.make || null) as string;
+    const model: string = (req?.query?.model || null) as string;
+
+    const miles = parseInt(req?.query?.miles as string) || 50;
+
+    let errorMsg = "";
+
+    if (!isValidVehicleType(vehicleType)) {
+      errorMsg = `Vehicle type not found (or) ${vehicleType} is not a valid type`;
+      return res.status(400).json({ data: [], error: errorMsg });
+    } else if (!make || !model) {
+      errorMsg = `Vehicle make / model not found (or) ${make} / ${model} is not a valid make/model`;
+      return res.status(400).json({ data: [], error: errorMsg });
+    }
+
+    const yearlyMakeModelConsumptionQuery =
+      buildAvgYearlyConsumptionForMakeAndModelQuery(make, model, vehicleType);
+
+    const { query, close } = await connect();
+
+    try {
+      const dbResponse = await query(yearlyMakeModelConsumptionQuery);
+
+      console.log(
+        `Retrieved ${dbResponse.rows.length} rows of yearly fuel consumption values 
+        for make: ${make} model: ${model} and vehicle_type: ${vehicleType}`,
+      );
+
+      const normalizedConsumptionValues = convertToBTUs(
+        dbResponse.rows,
+        miles,
+        vehicleType,
+      );
+
+      const data = normalizedConsumptionValues.map((row) => {
+        return {
+          year: row.year,
+          consumption: row.consumption,
+        };
+      });
+
+      return res.status(200).json({ data, error: "" });
+    } catch (error) {
+      console.log(
+        `Error while retrieving yearly emissions for make: ${make}, model: ${model} 
+        and vehicle_type ${vehicleType} - ${error}`,
+      );
+
+      errorMsg = `Unexpected error occurred while retrieving yearly emissions for make: 
+        ${make}, model: ${model} and vehicle_type ${vehicleType}`;
+
+      return res.status(500).json({ data: [], error: errorMsg });
+    } finally {
+      await close();
+    }
+  },
+);
 
 export default fuelConsumptionRouter;
